37a66566b1d2d25f68772a63c83a75a7ce0e0d16
Alan Alvarez <115573532+AlvarezNA@users.noreply.github.com>
2025-01-16 21:33:13 -0300

    first

---
+++ assets/skillsData.js
@@ -0,0 +1,62 @@
+export const skillList = [
+    {
+        skillName: 'HTML',
+        skillUrl: 'https://user-images.githubusercontent.com/31516195/170096301-84bb07bb-c807-46e4-a7d1-72e3f86c611c.png',
+    },
+    {
+        skillName: 'CSS',
+        skillUrl: 'https://user-images.githubusercontent.com/31516195/170096288-b6a601b7-7f5c-489e-be56-f9a7d61208f4.png',
+    },
+    {
+        skillName: 'JavaScript',
+        skillUrl: 'https://user-images.githubusercontent.com/31516195/170096309-ad1895af-57fc-4744-ad4c-35888c4f125b.png',
+    },
+    {
+        skillName: 'React',
+        skillUrl: 'https://user-images.githubusercontent.com/31516195/170096335-c6a18931-394d-444f-918a-51e1d052347b.png',
+    },
+    {
+        skillName: 'Redux',
+        skillUrl: 'https://user-images.githubusercontent.com/31516195/170096339-387cbc59-2630-458b-a2b7-a652245ea58a.png',
+    },
+    {
+        skillName: 'NodeJS',
+        skillUrl: 'https://user-images.githubusercontent.com/31516195/170096325-d548fb46-5806-41a1-8af6-43a1bdcc7f89.png',
+    },
+    {
+        skillName: 'TypeScript',
+        skillUrl: 'https://user-images.githubusercontent.com/31516195/170096347-7a6e9142-2aeb-4a1c-bcbb-da12a21d3aca.png',
+    },
+    {
+        skillName: 'Laravel',
+        skillUrl: 'https://user-images.githubusercontent.com/31516195/170096313-9e52f598-1d07-4689-916c-2b9ee3453227.png',
+    },
+    {
+        skillName: 'MongoDB',
+        skillUrl: 'https://user-images.githubusercontent.com/31516195/170096317-6eabc365-6fb1-41e2-b699-ea18f202a145.png',
+    },
+    {
+        skillName : 'Mysql',
+        skillUrl: 'https://user-images.githubusercontent.com/31516195/170096321-7dc0e110-e99b-49c2-831b-aa72ede1a82e.png'
+    },
+    {
+        skillName : 'Php',
+        skillUrl: 'https://user-images.githubusercontent.com/31516195/170097980-463780a1-6a86-4d8a-8ba4-dcc882fdf57a.png'
+    },
+    {
+        skillName : 'Vue.js',
+        skillUrl: 'https://user-images.githubusercontent.com/31516195/170096352-411826c2-3fcc-410a-bdfb-ce5119acf397.png'
+    },
+    {
+        skillName: 'GitHub',
+        skillUrl: 'https://user-images.githubusercontent.com/31516195/170096360-d1d5aa3f-4b3f-4d87-a5e2-2fd78b186b9d.png'
+    },
+    {
+        skillName: 'Trello',
+        skillUrl: 'https://user-images.githubusercontent.com/31516195/170096365-d42b3d89-d495-4f33-96b6-5cb25b712c4e.png'
+    },
+    {
+        skillName: 'Express',
+        skillUrl: 'https://user-images.githubusercontent.com/31516195/170096370-d809ad5c-67f7-42e4-9e79-6d97e8b5b1a8.png'
+    },
+];
